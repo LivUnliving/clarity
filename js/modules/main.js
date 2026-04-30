5f2d909f2f26f5add9482cc9ee8f5b7cf278fda4
MariFrixta <mscolari799@gmail.com>
2026-04-30 01:23:03 -0300

    fix: Consertando erros de digitação

--- js/modules/main.js
+++ js/modules/main.js
@@ -1,24 +1,20 @@
-// main.js
 import { gerarIframePorLink } from './logic.js';
-import { renderizarPlayer } from '../../js/ui.js';
-// Link padrão
-const LINK_PADRAO = "https://open.spotify.com/playlist/3Z8RxqQVHdxyFLt5Ewe1FX?si=7a7b413ffdc04fcf"; 
+import { renderizarPlayer } from './ui.js';
+
+const LINK_PADRAO = "https://open.spotify.com/playlist/37i9dQZF1E4yzIYT5qw6EX?si=a14d0feafa614a8d"; 
 
 let estado = {
     playerAtivo: true,
     htmlIframe: gerarIframePorLink(LINK_PADRAO)
 };
 
-// 3. FUNÇÃO CENTRAL DE ATUALIZAÇÃO (Padrão State -> Render)
 function atualizarEstado(novoEstado) {
     estado = { ...estado, ...novoEstado }; 
     renderizarPlayer(estado);              
 }
 
-// 4. INICIALIZAÇÃO: Renderiza o player padrão logo que a página carrega
 renderizarPlayer(estado);
 
-// 5. EVENTOS (Permanece igual, permitindo a troca pelo usuário)
 const formulario = document.getElementById('form-link');
 const inputUrl = document.getElementById('urlUsuario');
 
